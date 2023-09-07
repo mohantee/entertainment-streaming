3e6ea56031b73afe1ef7bf8f3ad9ef89fa8c84ed
Saurav Mohanty <sauravmohantee@gmail.com>
2023-09-08 02:09:29 +0530

    fix: home page search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,32 @@ import useShowsStore from "../store/shows";
 import MediaGrid from "../components/MediaGrid/MediaGrid";
 import "./Home.css";
 import MediaCard from "../components/MediaCard/MediaCard";
+import { useOutletContext } from "react-router-dom";
 
 const Home = () => {
-  const shows = useShowsStore((state) => state.shows);
-  const trendingShows = shows.filter((show) => show.isTrending);
+  const unfilteredShows = useShowsStore((state) => state.shows);
+  const [searchValue] = useOutletContext<[searchValue: string]>();
+  const filteredShowsTrending = unfilteredShows.filter(
+    (show) =>
+      show.title.toLowerCase().includes(searchValue.toLowerCase()) &&
+      show.isTrending,
+  );
+
+  const filteredShows = unfilteredShows.filter((show) =>
+    show.title.toLowerCase().includes(searchValue.toLowerCase()),
+  );
 
   return (
     <div className="flow home-layout">
       <h2 className="fs-700">Trending</h2>
       <MediaGrid slider>
-        {trendingShows.map((show) => (
+        {filteredShowsTrending.map((show) => (
           <MediaCard key={show.title} size="lg" show={show} />
         ))}
       </MediaGrid>
       <h2 className="fs-700">Recommended for you</h2>
       <MediaGrid>
-        {shows.map((show) => (
+        {filteredShows.map((show) => (
           <MediaCard key={show.title} size="md" show={show} />
         ))}
       </MediaGrid>
