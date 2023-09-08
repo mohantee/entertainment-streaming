c4f16dce3c3a8ddca6da3d9b29a5c532733ab25c
Saurav Mohanty <sauravmohantee@gmail.com>
2023-09-08 12:18:26 +0530

    fix: search icon aria hidden

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -25,7 +25,7 @@ const SearchBox = (props: SearchBoxProps) => {
 
   return (
     <div className="search-box-group">
-      <IconSearch className="search-icon" />
+      <IconSearch className="search-icon" aria-hidden="true" />
       <label htmlFor="search" className="sr-only">
         Search for Movies or TV series
       </label>
